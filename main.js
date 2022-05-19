a247929a0298d5785c635ce1662f9064308f018b
Tamanna-Kalra <83012226+Tamanna-Kalra@users.noreply.github.com>
2022-05-19 12:40:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier(https://teachablemachine.withgoogle.com/models/AGf55nRtD/', modelReady);
+}
+
+function modelReady(){
+  classifier.classify( gotResults);
+}
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    random_number_r = Math.floor(Math.random() * 255) + 1;
+    random_number_g = Math.floor(Math.random() * 255) + 1;
+    random_number_b = Math.floor(Math.random() * 255) + 1;
+
+    document.getElementById("result_label").innerHTML = 'I can hear - '+ results[0].label;
+    document.getElementById("result_confidence").innerHTML = 'Accuracy - '+ (results[0].confidence*100).toFixed(2)+" %";
+    document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+    document.getElementById("result_confidence").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+
+    img = document.getElementById('animal1') 
+    img1 = document.getElementById('animal2')
+    img2 = document.getElementById('animal3')
+    img3 = document.getElementById('animal4')
+    img4 = document.getElementById('sound5')
+
+    if (results[0].label == "Bark") {
+      img.src = 'dog - animal 1.jpg';
+    } else if (results[0].label == "Meow") {
+      img1.src = 'cat - animal 2.png';
+    } else if (results[0].label == "Roar") {
+      img2.src = 'lion - animal 3.png';
+    } else if (results[0].label == "Moo"){
+      img3.src = 'cow - animal 4.png';
+    } else {
+      img4.src = 'speaker - backgroung noise.jpg';
+    }
+  }
+}
